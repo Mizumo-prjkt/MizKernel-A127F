43df2375b50308303adfbede8451e3a5dfbb247b
Mizumo-prjkt <bluepearl394@gmail.com>
2025-06-19 21:09:45 +0800

    fix: uncomment conditional compilation directive in sel_write_enforce function

--- security/selinux/selinuxfs.c
+++ security/selinux/selinuxfs.c
@@ -194,7 +194,7 @@ static ssize_t sel_write_enforce(struct file *file, const char __user *buf,
 		if (!selinux_enforcing)
 			call_lsm_notifier(LSM_POLICY_CHANGE, NULL);
 	}
-// #endif
+#endif
 // ] SEC_SELINUX_PORTING_COMMON
 	length = count;
 out:
