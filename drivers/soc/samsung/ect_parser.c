86e3f64a5b112486b3864db9b14d74eef50a5bcf
Mizumo-prjkt <bluepearl394@gmail.com>
2025-06-20 01:23:08 +0800

    feat: add arg_freqs.h header and update extern declarations in cpufreq and ect_parser

--- drivers/soc/samsung/ect_parser.c
+++ drivers/soc/samsung/ect_parser.c
@@ -11,6 +11,10 @@
 #include <linux/module.h>
 #include <linux/vmalloc.h>
 
+// MizProject/Mizumo-prjkt specific workaround on issue of
+// reference error
+#include <soc/samsung/arg_freqs.h>
+
 #define ALIGNMENT_SIZE	 4
 
 #define S5P_VA_ECT (VMALLOC_START + 0xF6000000 + 0x02D00000)
