0bfbb098ac72d566e5ae09db6a8ef93bedb91e15
Mizumo-prjkt <bluepearl394@gmail.com>
2025-06-19 20:27:12 +0800

    feat: add ksu_symlink function to handle kernel directory symlink creation
    ---------------
    
    I am a bad programmer when implementing stuff i guess
    
    ---------------
    Commit by:
    Mizu/Mizumo-prjkt (mizproject@proton.me)

--- runner.sh
+++ runner.sh
@@ -12,6 +12,19 @@ export LLVM=1
 # make ARCH=arm64 exynos850-a12snsxx_defconfig
 # make ARCH=arm64 -j64
 
+ksu_symlink() {
+    if [ ! -d "KernelSU-Next/kernel" ]; then
+        echo "Empty KernelSU-Next/kernel directory, abort as extreme error"
+        exit 9
+    fi
+    ln -sf $(pwd)/KernelSU-Next/kernel $(pwd)/drivers/kernelsu
+    if [ $? -ne 0 ]; then
+        echo "Failed to create symlink for KernelSU-Next/kernel"
+        exit 1
+    fi
+}
+
+
 submodule_summon() {
     git submodule update --init --recursive
 }
@@ -28,6 +41,9 @@ menuconfig_summon() {
 
 case "$1" in
     "build")
+        # Use ksu_symlink as always since object files are permanently configured
+        # and to avoid future errors relating "missing file or foler"
+        ksu_symlink
         if [ "$2" == "-submodule_summon" ]; then
             submodule_summon
         fi
